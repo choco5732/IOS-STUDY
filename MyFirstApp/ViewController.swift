aacd4d8966e6f51e2bd8503b644ee1ea68dfc011
choco5732 <choco5732@gmail.com>
2024-09-25 16:23:51 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by jyjang on 2024.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemCyan
+    }
+
+
+}
+
